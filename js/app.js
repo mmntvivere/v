2e6849d374289047cb0c8b500934898ccb04e778
Christian Palacios <maple.feb14@gmail.com>
2021-11-29 21:09:51 +0800

    v 1.00 - Added Content

---
+++ js/app.js
@@ -0,0 +1,74 @@
+$(function(){
+    $('#aboutW').draggable({
+        containment: 'parent'
+    });
+    $('#diemW').draggable({
+        containment: 'parent'
+    });
+    $('#noctemW').draggable({
+        containment: 'parent'
+    });
+    $('#contactsW').draggable({
+        containment: 'parent'
+    });
+});
+
+let about = document.getElementById("aboutW");
+let diem = document.getElementById("diemW");
+let noctem = document.getElementById("noctemW");
+let contacts = document.getElementById("contactsW");
+
+function openWindow(window){
+    switch(window) {
+        case window = 'about':
+            about.style.width = '30em' ;
+            about.style.zIndex = 1;
+            break;
+        case window = 'diem':
+            diem.style.width = '35em';
+            diem.style.zIndex = 1;
+            break;
+        case window = 'noctem':
+            noctem.style.width = '35em';
+            noctem.style.zIndex = 1;
+            break;
+        case window = 'contacts':
+            contacts.style.width = '21em'
+            contacts.style.zIndex = 1;
+            break;
+        default:
+          // code block
+    }
+};
+
+function closeWindow(window){
+    switch(window) {
+        case window = 'about':
+            about.style.zIndex = -1;
+            about.style.left = 0;
+            about.style.top = 0;
+            about.style.width = 0;
+            break;
+        case window = 'diem':
+            diem.style.zIndex = -1;
+            diem.style.left = 0;
+            diem.style.top = 0;
+            diem.style.width = 0;
+            break;
+        case window = 'noctem':
+            noctem.style.zIndex = -1;
+            noctem.style.left = 0;
+            noctem.style.top = 0;
+            noctem.style.width = 0;
+            break;
+        case window = 'contacts':
+            contacts.style.zIndex = -1;
+            contacts.style.left = 0;
+            contacts.style.top = 0;
+            contacts.style.width = 0;
+            break;
+        default:
+          // code block
+    }
+};
+
